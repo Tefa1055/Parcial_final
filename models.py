248794cf2b3fb17adbb70cd8402b5d586e00fa45
labsis <gbeltran65@ucatolica.edu.co>
2025-06-12 04:34:22 -0500

    a

--- models.py
+++ models.py
@@ -5,10 +5,13 @@ from pydantic import BaseModel
 
 # Modelo base para los usuarios
 class UsuarioBase(SQLModel):
-    title: str = Field(index=True)
-    developer: Optional[str] = None
-    release_year: Optional[int] = None
+    Nombre: str = Field(index=True)
+    id: int = Field(index=True)
 
-# Modelo de tabla para los juegos (para la base de datos)
+# Modelo de tabla para los usuarios (para la base de datos)
 class Usuario(UsuarioBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+
+class Mascota(SQLModel):
+    Nombre:str = Field(..., min_length=3, max_length=20)
+    id_Mascota: int = Field(default=None, primary_key=True)
